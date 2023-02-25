da6b916db17b65ace7eb37a66772025cf246c37c
Bappy71 <19103076@iubat.edu>
2023-02-26 05:07:06 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>hi there !!</h2>
+      <h4>This is md Khairul Bashar</h4>
+      <h6>ID : 19103076 </h6>
+    </div>
+  );
+}
+
+export default App;
